4dc57016659095f93d92dda682ca05133f261c6f
Valentin-Petitniot <99020098+Valentin-Petitniot@users.noreply.github.com>
2023-05-29 11:17:02 +0200

    Initial commit

---
+++ src/main/java/com/example/javafxlearning/Controller.java
@@ -0,0 +1,12 @@
+package com.example.javafxlearning;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+public class Controller {
+
+    @FXML
+
+}
